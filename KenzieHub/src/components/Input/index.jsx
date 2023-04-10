602961d3ca3a3c33aba3f9bb96b0fd4a6c63fa06
Caio <caiob1305@gmail.com>
2023-04-10 06:41:38 -0300

    Additions

---
+++ KenzieHub/src/components/Input/index.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export const InputForm = () => {
+  return (
+    <div>
+      <label htmlFor=""></label>
+      <input type="text" />
+      <p>error</p>
+    </div>
+  )
+}
--- KenzieHub/src/pages/Register/index.jsx
+++ KenzieHub/src/pages/Register/index.jsx
@@ -1,9 +1,35 @@
 import React from "react"
+import { useNavigate } from "react-router-dom"
+
+import api from "../../services/api"
 
 export const RegisterPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm()
+
+  const navigate = useNavigate()
+
+  const handleRegister = async (data) => {
+    try {
+      console.log(data)
+      await api.post("/users", data)
+      navigate("/home")
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
-    <div>
-      <h1>Register</h1>
-    </div>
+    <main>
+      <section>
+        <p>Registro</p>
+        <form onSubmit={handleSubmit(handleRegister)}>
+          <input type="text" />
+        </form>
+      </section>
+    </main>
   )
 }
